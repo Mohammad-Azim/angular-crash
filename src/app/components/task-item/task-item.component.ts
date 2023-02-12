5a01eeb80d4ea90e9068b9e32f7790bd06b98912
Mohammad-Azim <mohammad.azim@optimumpartners.co>
2023-02-12 15:15:47 +0200

    finished

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,19 +1,22 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { faTrash  } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent {
   trashIcon = faTrash;
   @Input() task!: Task;
-  @Output() onDeleteTask:EventEmitter<Task> = new EventEmitter()
-  onDelete(task : Task){
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleRemainder: EventEmitter<Task> = new EventEmitter();
+
+  onDelete(task: Task) {
     this.onDeleteTask.emit(task);
   }
-
+  onToggle(task: Task) {
+    this.onToggleRemainder.emit(task);
+  }
 }
-
